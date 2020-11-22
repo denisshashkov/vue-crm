dcb2a72319ea579c795020bac7adcaade967f260
denisshashkov <shashkov-56@mail.ru>
2020-11-22 16:14:22 +0300

    add message plugins

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Vuelidate from "vuelidate";
 import App from "./App.vue";
 import dateFilter from "@/filters/date.filter";
+import messagePlugin from "@/utils/message.plugin";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
@@ -9,6 +10,7 @@ import "materialize-css/dist/js/materialize.min";
 
 Vue.config.productionTip = false;
 
+Vue.use(messagePlugin);
 Vue.use(Vuelidate);
 Vue.filter("date", dateFilter);
 
